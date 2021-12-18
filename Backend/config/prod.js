71cb87ec3f58d119aadfcb01887e3431e039fc4b
Michael Uzan <looply3@gmail.com>
2021-12-18 18:05:53 +0200

    configure atlas DB on heroku deployment

--- Backend/config/prod.js
+++ Backend/config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  // 'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://looply:Deadmau5@cluster0.ugp8z.mongodb.net/rewire?retryWrites=true&w=majority'
 }
